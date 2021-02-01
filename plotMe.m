04aee47449542b3655416151cca4aefd9ff633c2
Jo Carpenter <jordannc@stud.ntnu.no>
2021-02-01 14:46:26 -0500

    variance explained

--- plotMe.m
+++ plotMe.m
@@ -4,6 +4,9 @@ warning('off', 'all')
 pred_val = out.measures.mu.RH;
 data_val = out.measures.mu.HD;
 
+xref = out.model.fitParams.xref;
+yref = out.model.fitParams.yref;
+
 % reshape MVL (scaling factor)
 data_MVL = out.measures.MVL.HD;
 data_MVL(isnan(data_MVL))=0;
@@ -36,20 +39,26 @@ binX = out.info.bin.X;
 binY = out.info.bin.Y;
 
 %% PLOT
-figure; hold on; set(gcf,'color','w');
-set(gca, 'visible', 'off')
-imagescwithnan(out.data.rxyS,jet,[.7 .5 .7])% colorbar
+% figure; set(gcf,'color','w');
+hold on;
+% set(gca, 'visible', 'off')
+imagescwithnan(out.data.rxy,jet,[.7 .5 .7])% colorbar
 alpha(0.2) 
 brighten(.6)
 xlim([0 11]); ylim([0 11]);
 
 % plot data vectors
 modelVecs = quiver(binX, binY, uprime_data, vprime_data, 0);
-set(modelVecs, 'Color', 'blue', 'AutoScale', 'off', 'LineWidth',1)
+set(modelVecs, 'Color', 'blue', 'AutoScale', 'off', 'LineWidth',.5)
 
 % plot model vectors
 modelVecs = quiver(binX, binY, uprime, vprime, 0);
-set(modelVecs, 'Color', 'r', 'AutoScale', 'off', 'LineWidth',1)
+set(modelVecs, 'Color', 'r', 'AutoScale', 'off', 'LineWidth',.5)
+
+% put in the reference point if its not distance
+if xref>0 & xref<150 & yref>0 & yref<150
+    scatter(xref,yref,[10],'k','filled')
+end
 
 % make the figure a square
 pbaspect([1 1 1])
